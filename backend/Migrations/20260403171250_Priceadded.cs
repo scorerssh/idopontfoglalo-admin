1d932726fbc313ec6d0561bca7e1d91913a36d9c
Szilárd Plachi <avp28202@outlook.hu>
2026-04-03 19:13:09 +0200

    commuit

---
+++ backend/Migrations/20260403171250_Priceadded.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApartManBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Priceadded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Price",
+                table: "rooms",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "rooms");
+        }
+    }
+}
