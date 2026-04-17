25bf52ed2d6a284b82e6cee170254a6124c5e8ad
Szilárd Plachi <avp28202@outlook.hu>
2026-04-18 00:05:31 +0200

    vv

--- backend/ResponseModel/Room/RoomPublicResponse.cs
+++ backend/ResponseModel/Room/RoomPublicResponse.cs
@@ -1,8 +1,14 @@
+using ApartManBackend.ResponseModel.AgePriceTier;
+using ApartManBackend.ResponseModel.RoomPriceTier;
+
 namespace ApartManBackend.ResponseModel.Room
 {
     public class RoomPublicResponse
     {
         public string Name { get; set; } = null!;
         public int Price { get; set; }
+        public List<RoomPriceTierResponse>? RoomPriceTiers { get; set; }
+        public List<AgePriceTierResponse>? AgePriceTiers { get; set; }
     }
+
 }
