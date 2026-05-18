e63c1c817e8fcccc0d2e8b48c549a0e863111dc4
Prometheus <dolfismsh@gmail.com>
2026-05-19 01:18:27 +0200

    axios update to dynamic browser url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: typeof window !== 'undefined' ? window.location.origin : import.meta.env.VITE_API_URL,
   timeout: 15000,
   withCredentials: true,
   headers: {
